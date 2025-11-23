8622d73617946f87de1ebe5996910514dd4a7030
Clipstone <declandavidwww@gmail.com>
2025-11-23 11:34:02 +0000

    Add wallet and purchase placeholders to store UI
    Introduces styled placeholder components for wallet and purchases sections in the store, including sample data and improved CSS for layout and appearance. Enhances user experience by providing visual structure and context for future dynamic content.

--- MalevolentWebsite/resources/views/components/content/store/wallet.blade.php
+++ MalevolentWebsite/resources/views/components/content/store/wallet.blade.php
@@ -1,3 +1,12 @@
 <div class="wallet">
-
+    <div class="wallet-placeholder">
+        <div class="title">
+            <i class="fa-solid fa-wallet"></i>
+            £10,000,000
+        </div>
+        <div class="description">
+            You can use the money you make from in-game or by topping up your bank to buy things from the store to enhance your overall gameplay for you and your friends.
+        </div>
+        <a>Top-up</a>
+    </div>
 </div>
